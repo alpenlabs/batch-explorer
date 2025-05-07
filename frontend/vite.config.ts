819474011813f70f1acd8f9923d84c2a136aa236
krsnapaudel <dilli@alpenlabs.io>
2025-05-07 17:56:44 +0545

    add allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["@tanstack/react-query"],
+  },
+  server: {
+    allowedHosts: [
+        "batches.testnet.alpenlabs.io",
+        "batches.testnet-staging.stratabtc.org",
+    ],
+  },
+});
